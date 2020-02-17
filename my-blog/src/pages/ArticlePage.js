e111836ad2f64470311cd57a4c609a493335cf67
Petros Shumie <shumie.peter@gmail.com>
2020-02-17 12:41:59 -0500

    added components for vote updating from frontend to server side

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import articleContent from "./article-content";
 import ArticlesList from '../components/ArticlesList';
+import CommentsList from '../components/CommentsList';
+import UpvotesSection from '../components/UpvotesSection';
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = ({ match }) => {
@@ -25,10 +27,11 @@ const ArticlePage = ({ match }) => {
   return (
     <>
       <h1>{article.title}</h1>
-      <p>This post has been upvoted {articleInfo.upvotes} times.</p>
+      <UpvotesSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo} />
       {article.content.map((paragraph, key) => (
       <p key={key}>{paragraph}</p>
       ))}
+      <CommentsList comments={articleInfo.comments} />
       <h3>Other Articles:</h3>
       <ArticlesList articles={otherArticles} />
     </>
