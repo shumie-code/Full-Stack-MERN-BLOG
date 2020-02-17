71768259682e4f7c2706ea11fb3e171db425c5c6
Petros Shumie <shumie.peter@gmail.com>
2020-02-16 20:14:04 -0500

    Added backend support for blog creation with MongoDB for persiting data from the client side.

---
+++ my-blog-backend/src/server.js
@@ -0,0 +1,71 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { MongoClient } from "mongodb";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/api/articles/:name", async (req, res) => {
+  try {
+    const articleName = req.params.name;
+
+    const client = await MongoClient.connect("mongodb://localhost:27017", {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    const db = client.db("my-blog");
+
+    const articlesInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    res.status(200).json(articlesInfo);
+
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to the db", error });
+  }
+});
+
+app.post("/api/articles/:name/upvote", async (req, res) => {
+  try {
+    const articleName = req.params.name;
+
+    const client = await MongoClient.connect("mongodb://localhost:27017", {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    const db = client.db("my-blog");
+
+    const articlesInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    await db.collection("articles").updateOne(
+      { name: articleName },
+      {
+        $set: {
+          upvotes: articlesInfo.upvotes + 1
+        }
+      }
+    );
+    const updatedArticleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+
+    res.status(200).json(updatedArticleInfo);
+
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to db", error });
+  }
+});
+
+app.post("/api/articles/:name/add-comment", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+
+  articlesInfo[articleName].comments.push({ username, text });
+  res.status(200).send(articlesInfo[articleName]);
+});
+
+app.listen(8000, () => console.log("Listening on port 8000"));
---
+++ my-blog/src/pages/AboutPage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <>
+         <h1>About Me</h1>
+        <p> Proin et tortor lacus. 
+            Duis ornare ex quis leo dignissim lacinia. 
+            Duis faucibus consectetur ex, aliquet dapibus sem. Vestibulum sit amet viverra ligula. Donec iaculis orci lacus, ac placerat augue maximus sed. 
+            Cras vel semper mi, et tristique libero.
+            Duis dignissim gravida velit lobortis auctor. Sed consequat dapibus orci, eu porttitor nisi commodo eget. Proin a mauris non neque euismod convallis.  
+        </p>
+
+         <p> Proin et tortor lacus. 
+             Duis ornare ex quis leo dignissim lacinia. 
+             Duis faucibus consectetur ex, aliquet dapibus sem. Vestibulum sit amet viverra ligula. Donec iaculis orci lacus, ac placerat augue maximus sed. 
+             Cras vel semper mi, et tristique libero.
+             Duis dignissim gravida velit lobortis auctor. Sed consequat dapibus orci, eu porttitor nisi commodo eget. Proin a mauris non neque euismod convallis.  
+        </p>      
+
+        <p> Proin et tortor lacus. 
+             Duis ornare ex quis leo dignissim lacinia. 
+             Duis faucibus consectetur ex, aliquet dapibus sem. Vestibulum sit amet viverra ligula. Donec iaculis orci lacus, ac placerat augue maximus sed. 
+             Cras vel semper mi, et tristique libero.
+             Duis dignissim gravida velit lobortis auctor. Sed consequat dapibus orci, eu porttitor nisi commodo eget. Proin a mauris non neque euismod convallis.  
+        </p>
+    </>
+  
+);
+
+export default AboutPage;
