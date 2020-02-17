e44e44f50e0c8c5b60913eae3efe458152cba71c
Petros Shumie <shumie.peter@gmail.com>
2020-02-17 10:22:08 -0500

    Added React hooks useEffect and useState for managing dataing retireval from the mongo db connected on the backend via express servers through a docker container.

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import articleContent from "./article-content";
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
@@ -7,12 +7,19 @@ const ArticlePage = ({ match }) => {
   const name = match.params.name;
    const article = articleContent.find(article => article.name === name);
 
+   const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    setArticleInfo({ upvotes: Math.ceil(Math.random() * 10) })
+  }, [name]);
+
   if (!article) return <NotFoundPage />
 
   const otherArticles = articleContent.filter(article => article.name !== name);
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This post has been upvoted {articleInfo.upvotes} times.</p>
       {article.content.map((paragraph, key) => (
       <p key={key}>{paragraph}</p>
       ))}
@@ -20,5 +27,5 @@ const ArticlePage = ({ match }) => {
       <ArticlesList articles={otherArticles} />
     </>
   );
-}
+ }
 export default ArticlePage;
